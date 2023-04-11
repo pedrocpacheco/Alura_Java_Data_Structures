553f17c7f7de041070cdbc9cecf6e169b3dc34bc
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-11 14:26:54 -0300

    feat: looping in the numberList using for each

--- Course_6_Arrays_Wrappers_Lambdas/src/arrayLists/ForLoopArray.java
+++ Course_6_Arrays_Wrappers_Lambdas/src/arrayLists/ForLoopArray.java
@@ -5,16 +5,27 @@ import java.util.ArrayList;
 public class ForLoopArray {
     public static void main(String[] args) {
         
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        // Creating a ArrayList of Integers
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
         
+        // Creating 2 ints
         int grade1 = 10;
         int grade2 = 9;
 
-        list.add(grade1);
-        list.add(grade2);
+        // Adding the Ints to the numberList
+        numberList.add(grade1);
+        numberList.add(grade2);
 
-        for(int i = 0; i < list.size(); i++){
-            System.out.println("The Current grade is: " + list.get(i));
+        // Looping the list using a For Loop        
+        for(int i = 0; i < numberList.size(); i++){
+            System.out.println("Using For Loop | The Current grade is: " + numberList.get(i));
+        }
+
+        System.out.println("---------------------------------");
+
+        // Looping the list using For Each
+        for(Integer grade : numberList){
+            System.out.println("Using For Each | The Current grade is: " + grade);
         }
 
     }
