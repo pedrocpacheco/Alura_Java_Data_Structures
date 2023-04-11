8e698e0457c6f8ae2248510f46e5353faf4c614c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-11 13:51:31 -0300

    first commit

---
+++ Course_6_Arrays_Wrappers_Lambdas/src/arrayLists/CreateAddRemove.java
@@ -0,0 +1,36 @@
+package arrayLists;
+
+import java.util.ArrayList;
+
+public class CreateAddRemove {
+    public static void main(String[] args) {
+        
+        // Creating the nameList
+        ArrayList nameList = new ArrayList();
+
+        // Priting nameList size as 0
+        System.out.println(nameList.size());
+
+        // Creating name variables
+        String nameMale = "Pedro";
+        String nameFemale = "Luane";
+
+        // Adding names to nameList
+        nameList.add(nameMale);
+        nameList.add(nameFemale);
+
+        // Priting especific names from nameList
+        System.out.println("The Male is: " + nameList.get(0));
+        System.out.println("The Female is: " + nameList.get(1));
+
+        // Priting the nameList size after adding names:
+        System.out.println(nameList.size());
+
+        // Removing name from nameList using it's id
+        nameList.remove(0); // Pedro Removed
+
+        // Printing nameList size after removing
+        System.out.println(nameList.size());
+    }
+    
+}
