6ea840105734c3d150eca048047a72d43433b182
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-11 14:57:39 -0300

    feat: name search and answer by using the .contains

---
+++ Course_6_Arrays_Wrappers_Lambdas/src/arrayLists/Class.java
@@ -0,0 +1,38 @@
+package arrayLists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Class {
+    public static void main(String[] args) {
+        
+        // Creating Name List
+        ArrayList<String> nameList = new ArrayList<String>();
+
+        //Adding names to nameList
+        nameList.add("Pedro");
+        nameList.add("Luane");
+
+        // Creating Scanner to Name Search
+        Scanner sc = new Scanner(System.in);
+
+        // Asking the User to Type the Name he's looking for
+        System.out.println("Search in the Name list: ");
+        String nameSearched = sc.nextLine();
+        sc.close();
+
+        // Dinamic answering if the name is or is not in the nameList
+        if(nameList.contains(nameSearched))
+            System.out.println("\nName Founded!\n");
+        else  
+            System.out.println("\nName Not Founded!\n");
+
+        // Showing all the name using a for loop
+        System.out.println("All the Names in The Name List Are:\n");
+        for(String name : nameList){
+            System.out.println("Current Name Is: " + name);
+        }
+
+
+    }
+}
