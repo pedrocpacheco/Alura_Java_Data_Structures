d9aafa09224e1661594ee7c36c3b6fd17302bb19
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-11 16:07:48 -0300

    feat: using linked lists and seeing it's resemblance with ArrayList

---
+++ linkedList/Contains.java
@@ -0,0 +1,51 @@
+package linkedList;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Contains {
+    public static void main(String[] args) {
+        
+        // Creating Name List
+        LinkedList<String> nameList = new LinkedList<String>();
+
+        //Adding names to nameList
+        nameList.add("Pedro");
+        nameList.add("Luane");
+
+        // Creating Scanner to Name Search
+        Scanner sc = new Scanner(System.in);
+
+        // Asking the User to Type the Name he's looking for
+        System.out.println("Search in the Name list: ");
+        String nameSearched = sc.nextLine();
+
+        // Dinamic answering if the name is or is not in the nameList
+        if(nameList.contains(nameSearched))
+            System.out.println("\nName Founded!\n");
+            System.out.println("Do you want the remove that name do the List? \nPress 1: Yes | Other: No\n");
+            int optionRemove = sc.nextInt();
+            sc.close();
+
+            if(optionRemove == 1){
+                nameList.remove(nameSearched);
+            }
+        else  
+            System.out.println("\nName Not Founded!\n");
+
+            System.out.println("Do you want the add that name do the List? \nPress 1: Yes | Other: No\n");
+            int optionAdd = sc.nextInt();
+            sc.close();
+
+            if(optionAdd == 1){
+                nameList.add(nameSearched);
+            }
+        // Showing all the name using a for loop
+        System.out.println("All the Names in The Name List Are:\n");
+        for(String name : nameList){
+            System.out.println("Current Name Is: " + name);
+        }
+
+
+    }
+}
