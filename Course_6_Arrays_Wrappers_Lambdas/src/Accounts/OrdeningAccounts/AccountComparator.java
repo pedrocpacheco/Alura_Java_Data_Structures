9cf456c7567628deb23c3b4d967f95e495d19c05
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-12 14:58:30 -0300

    feat(OrderingAccounts): ordering accounts by using a AccountComparator that Implements Comparator

---
+++ Course_6_Arrays_Wrappers_Lambdas/src/Accounts/OrdeningAccounts/AccountComparator.java
@@ -0,0 +1,14 @@
+package Accounts.OrdeningAccounts;
+
+import java.util.Comparator;
+
+import Accounts.Account;
+
+public class AccountComparator implements Comparato<Account> {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    }
+}
