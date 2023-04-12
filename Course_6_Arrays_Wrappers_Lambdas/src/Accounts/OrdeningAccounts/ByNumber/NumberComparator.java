5abe4105b723fa418e69900aceb59ab9ee53c48e
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-12 16:00:29 -0300

    refactor(NumberComparator.compare): using Integer.compare() to compare the 2 account numbers

--- Course_6_Arrays_Wrappers_Lambdas/src/Accounts/OrdeningAccounts/ByNumber/NumberComparator.java
+++ Course_6_Arrays_Wrappers_Lambdas/src/Accounts/OrdeningAccounts/ByNumber/NumberComparator.java
@@ -8,14 +8,20 @@ public class NumberComparator implements Comparator<Account> {
 
     @Override
     public int compare(Account ac1, Account ac2) {
-        if(ac1.getNumber() < ac2.getNumber()){
-            return -1;
-        }
+        // Smart Way to do The Number Comparation
+        return Integer.compare(ac1.getNumber(), ac2.getNumber());
+        
+        // Half Way to do The Number Comparation
+        // return ac1.getNumber() - ac2.getNumber();
 
-        if(ac1.getNumber() > ac2.getNumber()){
-            return 1;
-        }
+        // Dumb Way to do The Number Comparation
+        // if(ac1.getNumber() < ac2.getNumber()){
+        //     return -1;
+        // }
 
-        return 0;
+        // if(ac1.getNumber() > ac2.getNumber()){
+        //     return 1;
+        // }
+        // return 0;
     } 
 }
