e104d5ced1872dc47462c43cd7ad836ff0012e4e
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 16:48:49 -0300

    feat: commenting the code, and using .get to acess a especific name

--- Course_8_Collections/src/ListsTests/ArrayListTeste.java
+++ Course_8_Collections/src/ListsTests/ArrayListTeste.java
@@ -1,33 +1,44 @@
 package ListsTests;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class ArrayListTeste {
     public static void main(String[] args) {
+        // Creating NameList
         List<String> nameList = new ArrayList<String>();
+    
+        // Adding items to List
         nameList.add("Pedro");
         nameList.add("Luane");
         nameList.add("Daniel");
         nameList.add("Rick");
         nameList.add("Daryl");
         
+        // ForEach printing all the names with caption
         for (String name : nameList) {
             System.out.printf("Current name is: %s\n", name);
         }
+        // Printing the Default Look
         System.out.println(nameList);
+        System.out.println("Printing the Espcific name: " + nameList.get(0));
 
-        nameList.remove("Pedro"); // Removing item by value
-        System.out.println(nameList);
+        // Removing item by value
+        nameList.remove("Pedro");
+        System.out.println(nameList); // Printing after removing
 
-        nameList.remove(1); // Removing item by index
-        System.out.println(nameList);
+        // Removing item by index
+        nameList.remove(1); 
+        System.out.println(nameList); // Printing after removing
 
+        // ForEach using ArrayList method
         nameList.forEach(name -> {System.out.println("This name is: " + name);});
+        
+        // Sorting the List
         Collections.sort(nameList);
-        System.out.println(nameList);
+        System.out.println(nameList); // Printing the sorted list
+
 
     }
 }
