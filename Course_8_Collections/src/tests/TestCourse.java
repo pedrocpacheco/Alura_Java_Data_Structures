ccfdd8aaa73c2cfff66c20ad15fa235c17e7aca7
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 17:39:08 -0300

    refactor(test): deleting the creation of a reference, doing in same line

--- Course_8_Collections/src/tests/TestCourse.java
+++ Course_8_Collections/src/tests/TestCourse.java
@@ -1,7 +1,5 @@
 package tests;
 
-import java.util.List;
-
 import classes.Course;
 import classes.Lesson;
 
@@ -9,17 +7,13 @@ public class TestCourse {
     public static void main(String[] args) {
         
         Course javaCollections = new Course("Mastering the Collections", "Rafael Ronqui");
-        List<Lesson> javaLessons = javaCollections.getLessons();   
-        
-        System.out.println(javaLessons);
-
-        javaLessons.add(new Lesson("Relashionship of Collections", 23));
-        
-        // Printing by reference
-        System.out.println(javaLessons);
-
-        // Printing by object method
         System.out.println(javaCollections.getLessons());
+
+        javaCollections.getLessons().add(new Lesson("Relashionship of Collections", 23));
+
+        System.out.println(javaCollections.getLessons());
+
+
 
     }
 }
