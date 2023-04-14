9b8aad2d209ae84eb5718e7bf4219ab9e887f436
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 18:51:27 -0300

    feat: sets don't have a order and only save 1 item of X value

--- Course_8_Collections/src/tests/TestStudent.java
+++ Course_8_Collections/src/tests/TestStudent.java
@@ -5,11 +5,26 @@ import java.util.Set;
 
 public class TestStudent {
     public static void main(String[] args) {
+
+        // Creating HashSet
         Set<String> students = new HashSet<String>();
+        
         students.add("Pedro");
         students.add("Henrique");
         students.add("Luane");
 
+        // Random Order of the Itens
         System.out.println(students);
+
+        students.add("Pedro"); // Don't add a same value
+        students.add("Daniel"); // Just Different
+        
+        // Different Order From Before
+        System.out.println(students);
+
+        for (String student : students) {
+            System.out.println("Student Name's: " + student);
+        }
+
     }
 }
