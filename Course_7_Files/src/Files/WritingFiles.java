d15134c837ce66c6ca89be997c8ca17d3521e1c6
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 13:12:33 -0300

    refactor(WritingFiles): changing the variables names to math the class name

--- Course_7_Files/src/Files/WritingFiles.java
+++ Course_7_Files/src/Files/WritingFiles.java
@@ -9,14 +9,14 @@ import java.io.OutputStreamWriter;
 public class WritingFiles {
     public static void main(String[] args) throws IOException{
     
-        OutputStream fileInputStream = new FileOutputStream("saida.txt"); 
-        OutputStreamWriter inputStreamReader = new OutputStreamWriter(fileInputStream); 
-        BufferedWriter bufferedWriter = new BufferedWriter(inputStreamReader); 
+        OutputStream fo = new FileOutputStream("saida.txt"); 
+        OutputStreamWriter osw = new OutputStreamWriter(fo); 
+        BufferedWriter bw = new BufferedWriter(osw); 
       
-        bufferedWriter.write("This is the printed message in the file");
-        bufferedWriter.newLine();
-        bufferedWriter.write("I broke a line.");
+        bw.write("This is the printed message in the file");
+        bw.newLine();
+        bw.write("I broke a line.");
         
-        bufferedWriter.close(); 
+        bw.close(); 
      }
 }
