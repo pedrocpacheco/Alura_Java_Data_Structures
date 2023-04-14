23a78f6bf5cb6ee5675cda21768267b6a558356c
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 16:33:42 -0300

    feat: using lambda inside a .forEach method

--- Course_8_Collections/src/ListsTests/ArrayListTeste.java
+++ Course_8_Collections/src/ListsTests/ArrayListTeste.java
@@ -9,10 +9,21 @@ public class ArrayListTeste {
         nameList.add("Pedro");
         nameList.add("Luane");
         nameList.add("Daniel");
-
+        nameList.add("Rick");
+        nameList.add("Daryl");
+        
         for (String name : nameList) {
             System.out.printf("Current name is: %s\n", name);
         }
+        System.out.println(nameList);
+
+        nameList.remove("Pedro"); // Removing item by value
+        System.out.println(nameList);
+        
+        nameList.remove(1);
+        System.out.println(nameList);
+
+        nameList.forEach(name -> {System.out.println("Percorrendo: " + name);});
         
     }
 }
