36386d56b3f82aeb85f1e51be945a802eaf5ac67
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-14 16:54:42 -0300

    fix: changing the name of the Class and Test

--- Course_8_Collections/src/ListOfObjects/Lesson.java
+++ Course_8_Collections/src/ListOfObjects/Lesson.java
@@ -1,7 +1,20 @@
 package ListOfObjects;
 
-public class AluraClassTest {
-    public static void main(String[] args) {
-        
+public class Lesson {
+    
+    private String owner;
+    private int time;
+
+    public Lesson(String owner, int time){
+        this.owner = owner;
+        this.time = time;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+    
+    public int getTime() {
+        return time;
     }
 }
--- Course_8_Collections/src/ListOfObjects/TestLesson.java
+++ Course_8_Collections/src/ListOfObjects/TestLesson.java
@@ -1,20 +1,7 @@
 package ListOfObjects;
 
-public class AluraClass {
-    
-    private String owner;
-    private int time;
-
-    public AluraClass(String owner, int time){
-        this.owner = owner;
-        this.time = time;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-    
-    public int getTime() {
-        return time;
+public class TestLesson {
+    public static void main(String[] args) {
+        
     }
 }
