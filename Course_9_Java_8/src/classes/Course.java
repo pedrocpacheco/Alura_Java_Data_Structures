81cf07d38cc8f3adf4d9d27efa4c502774d377dd
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-17 16:38:03 -0300

    feat: seeing the bunch of methods that the stream have

--- Course_9_Java_8/src/classes/Course.java
+++ Course_9_Java_8/src/classes/Course.java
@@ -17,4 +17,10 @@ public class Course {
         return students;
     }
 
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "(Course: " + getName() + " | Students: " + getStudents() + ")";
+    }
+
 }
--- Course_9_Java_8/src/tests/Courses/TestCourse.java
+++ Course_9_Java_8/src/tests/Courses/TestCourse.java
@@ -9,12 +9,25 @@ import classes.Course;
 public class TestCourse {
     public static void main(String[] args) {
         List<Course> courses = new ArrayList<>(); // Create list
-        courses.add(new Course("Java 8", 40)); // Add 1 item
-        courses.add(new Course("Java BDD", 15)); // Add 2 item
-        courses.add(new Course("Spring MVC", 25)); // Add 3 item
+        courses.add(new Course("Java 8", 110)); // Add 1 item
+        courses.add(new Course("Java BDD", 115)); // Add 2 item
+        courses.add(new Course("Spring MVC", 95)); // Add 3 item
 
         courses.sort(Comparator.comparing(Course::getStudents)); // Sorting by Method Reference
         courses.forEach(System.out::println); // ForEach -> Print | Using MR
+
+        System.out.println("<Filtering the List>");
+
+        // Learing Streams
+        courses.stream().filter(course -> course.getStudents() >= 100); // Don't delete all the -100 students courses
+        courses.forEach(System.out::println); // It print's all the courses
+
+        // Printing the filter
+        int sum = courses.stream().filter(course -> course.getStudents() >= 100)
+                        .mapToInt(Course::getStudents)
+                        .sum();
+
+        System.out.println("The Sum of the +100 Students Course's is: " + sum);
 
     }
 }
