f01b94f830c39be00b3d468008d730d37c4df949
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-17 15:11:39 -0300

    refactor: separing things by: before java 8 -> after java 8

--- Course_9_Java_8/src/classes/BeforeJava8/LengthComparator.java
+++ Course_9_Java_8/src/classes/BeforeJava8/LengthComparator.java
@@ -1,4 +1,4 @@
-package classes;
+package classes.BeforeJava8;
 
 import java.util.Comparator;
 
--- Course_9_Java_8/src/tests/BeforeJava8/OrderingStrings.java
+++ Course_9_Java_8/src/tests/BeforeJava8/OrderingStrings.java
@@ -1,20 +1,24 @@
-package tests;
+package tests.BeforeJava8;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import classes.LengthComparator;
+import classes.BeforeJava8.LengthComparator;
+
 
 public class OrderingStrings {
     public static void main(String[] args) {
         String[] a = new String[] {"Code's House", "Caelum", "Alura Online"}; 
         List<String> words = Arrays.asList(a);
 
+        // Creating a Comparator
         LengthComparator lengthComparator = new LengthComparator();
+        
+        // Using the Collections static method
+        Collections.sort(words, lengthComparator);
 
-        words.sort(lengthComparator);
         System.out.println(words);
 
     }
