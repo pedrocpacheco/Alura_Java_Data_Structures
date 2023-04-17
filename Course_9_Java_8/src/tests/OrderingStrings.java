431d27ea635716eb18029c7994bc1394ef5ec1fe
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-17 15:04:20 -0300

    test: ordering the code list with the length comparator

--- Course_9_Java_8/src/tests/OrderingStrings.java
+++ Course_9_Java_8/src/tests/OrderingStrings.java
@@ -5,12 +5,16 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import classes.LengthComparator;
+
 public class OrderingStrings {
     public static void main(String[] args) {
-        String[] a = new String[] {"Code's House", "Caelum", "Alura"}; 
+        String[] a = new String[] {"Code's House", "Caelum", "Alura Online"}; 
         List<String> words = Arrays.asList(a);
 
-        Collections.sort(words);
+        LengthComparator lengthComparator = new LengthComparator();
+
+        words.sort(lengthComparator);
         System.out.println(words);
 
     }
