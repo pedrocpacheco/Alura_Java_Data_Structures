fe987251c305b96d88446b48e9c94736878718b2
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-17 17:39:38 -0300

    feat: manipulating Dates

--- Course_9_Java_8/src/tests/Data/TestDates.java
+++ Course_9_Java_8/src/tests/Data/TestDates.java
@@ -1,7 +1,28 @@
 package tests.Data;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class TestDates {
     public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
+        LocalDate rioOlimpics = LocalDate.of(2026, Month.JUNE, 5);
+        System.out.println(rioOlimpics);
+
+        Period period = Period.between(today, rioOlimpics);
+        System.out.println(period.getDays());
+
+        LocalDate nextOlimpics = rioOlimpics.plusYears(4);
+        System.out.println(nextOlimpics);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         
+        String formattedValue = nextOlimpics.format(formatter);
+        System.out.println(formattedValue);
+
     }
 }
